70eaf417e172ffdc09350ffff35b58777b0a98d1
Oleg <Oleg>
2016-06-08 01:42:57 +0300

    minor commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WpfApplication1.Views;
 using WpfApplication1.Beheivors;
+using System.Collections.ObjectModel;
+using WpfApplication1.ViewModels;
+using WpfApplication1.Models;
 
 namespace WpfApplication1
 {
@@ -39,184 +42,191 @@ namespace WpfApplication1
         {
             InitializeComponent();
             shapeList = new List<Shape>();
-            RectangleView newRect = new RectangleView();
-//            UserControlCustomProperty.SetCanvasProperty(newRect,mainCanvas);
-            newRect.mainCanvas = this.mainCanvas;
-            mainCanvas.Children.Add(newRect);
-/*
-            this.MouseLeftButtonDown += new MouseButtonEventHandler(Window1_MouseLeftButtonDown);
-            this.MouseLeftButtonUp += new MouseButtonEventHandler(DragFinishedMouseHandler);
-            this.MouseMove += new MouseEventHandler(Window1_MouseMove);
-            this.MouseLeave += new MouseEventHandler(Window1_MouseLeave);
 
-            this.shapesCount = 0;
-            mainCanvas.PreviewMouseLeftButtonDown += new MouseButtonEventHandler(myCanvas_PreviewMouseLeftButtonDown);
-            mainCanvas.PreviewMouseLeftButtonUp += new MouseButtonEventHandler(DragFinishedMouseHandler);
-*/
+            //            UserControlCustomProperty.SetCanvasProperty(newRect,mainCanvas);
+            //            newRect.mainCanvas = this.mainCanvas;
+            //            mainCanvas.Children.Add(newRect);
+            /*
+                        this.MouseLeftButtonDown += new MouseButtonEventHandler(Window1_MouseLeftButtonDown);
+                        this.MouseLeftButtonUp += new MouseButtonEventHandler(DragFinishedMouseHandler);
+                        this.MouseMove += new MouseEventHandler(Window1_MouseMove);
+                        this.MouseLeave += new MouseEventHandler(Window1_MouseLeave);
+
+                        this.shapesCount = 0;
+                        mainCanvas.PreviewMouseLeftButtonDown += new MouseButtonEventHandler(myCanvas_PreviewMouseLeftButtonDown);
+                        mainCanvas.PreviewMouseLeftButtonUp += new MouseButtonEventHandler(DragFinishedMouseHandler);
+            */
         }
-
-        void myCanvas_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (selected)
-            {
-                selected = false;
-                if (selectedElement != null)
+        /*
+                void myCanvas_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
                 {
-                    aLayer.Remove(aLayer.GetAdorners(selectedElement)[0]);
-                    selectedElement = null;
-                }
-            }
-
-            if (e.Source != mainCanvas)
-            {
-                _isDown = true;
-                _startPoint = e.GetPosition(mainCanvas);
-
-                selectedElement = e.Source as UIElement;
-
-                _originalLeft = Canvas.GetLeft(selectedElement);
-                _originalTop = Canvas.GetTop(selectedElement);
-
-                aLayer = AdornerLayer.GetAdornerLayer(selectedElement);
-                aLayer.Add(new MyAdorner(selectedElement));
-                selected = true;
-                e.Handled = true;
-            }
-        }
-        void Window1_MouseLeave(object sender, MouseEventArgs e)
-        {
-            StopDragging();
-            e.Handled = true;
-        }
-
-        // Handler for drag stopping on user choise
-        void DragFinishedMouseHandler(object sender, MouseButtonEventArgs e)
-        {
-            StopDragging();
-            e.Handled = true;
-        }
-
-        // Method for stopping dragging
-        private void StopDragging()
-        {
-            if (_isDown)
-            {
-                _isDown = false;
-                _isDragging = false;
-            }
-        }
-
-        // Hanler for providing drag operation with selected element
-        void Window1_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (_isDown)
-            {
-                if ((_isDragging == false) &&
-                    ((Math.Abs(e.GetPosition(mainCanvas).X - _startPoint.X) > SystemParameters.MinimumHorizontalDragDistance) ||
-                    (Math.Abs(e.GetPosition(mainCanvas).Y - _startPoint.Y) > SystemParameters.MinimumVerticalDragDistance)))
-                    _isDragging = true;
-
-                if (_isDragging)
-                {
-                    Point position = Mouse.GetPosition(mainCanvas);
-                    double posY = position.Y - (_startPoint.Y - _originalTop);
-                    double posX = position.X - (_startPoint.X - _originalLeft);
-                    if (posY > 0 && posX > 0)
+                    if (selected)
                     {
-                        Canvas.SetTop(selectedElement, posY);
-                        Canvas.SetLeft(selectedElement, posX);
+                        selected = false;
+                        if (selectedElement != null)
+                        {
+                            aLayer.Remove(aLayer.GetAdorners(selectedElement)[0]);
+                            selectedElement = null;
+                        }
+                    }
+
+                    if (e.Source != mainCanvas)
+                    {
+                        _isDown = true;
+                        _startPoint = e.GetPosition(mainCanvas);
+
+                        selectedElement = e.Source as UIElement;
+
+                        _originalLeft = Canvas.GetLeft(selectedElement);
+                        _originalTop = Canvas.GetTop(selectedElement);
+
+                        aLayer = AdornerLayer.GetAdornerLayer(selectedElement);
+                        aLayer.Add(new MyAdorner(selectedElement));
+                        selected = true;
+                        e.Handled = true;
                     }
                 }
-            }
-        }
-
-        void Window1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (selected)
-            {
-                selected = false;
-                if (selectedElement != null)
+                void Window1_MouseLeave(object sender, MouseEventArgs e)
                 {
-                    aLayer.Remove(aLayer.GetAdorners(selectedElement)[0]);
+                    StopDragging();
+                    e.Handled = true;
+                }
+
+                // Handler for drag stopping on user choise
+                void DragFinishedMouseHandler(object sender, MouseButtonEventArgs e)
+                {
+                    StopDragging();
+                    e.Handled = true;
+                }
+
+                // Method for stopping dragging
+                private void StopDragging()
+                {
+                    if (_isDown)
+                    {
+                        _isDown = false;
+                        _isDragging = false;
+                    }
+                }
+
+                // Hanler for providing drag operation with selected element
+                void Window1_MouseMove(object sender, MouseEventArgs e)
+                {
+                    if (_isDown)
+                    {
+                        if ((_isDragging == false) &&
+                            ((Math.Abs(e.GetPosition(mainCanvas).X - _startPoint.X) > SystemParameters.MinimumHorizontalDragDistance) ||
+                            (Math.Abs(e.GetPosition(mainCanvas).Y - _startPoint.Y) > SystemParameters.MinimumVerticalDragDistance)))
+                            _isDragging = true;
+
+                        if (_isDragging)
+                        {
+                            Point position = Mouse.GetPosition(mainCanvas);
+                            double posY = position.Y - (_startPoint.Y - _originalTop);
+                            double posX = position.X - (_startPoint.X - _originalLeft);
+                            if (posY > 0 && posX > 0)
+                            {
+                                Canvas.SetTop(selectedElement, posY);
+                                Canvas.SetLeft(selectedElement, posX);
+                            }
+                        }
+                    }
+                }
+
+                void Window1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+                {
+                    if (selected)
+                    {
+                        selected = false;
+                        if (selectedElement != null)
+                        {
+                            aLayer.Remove(aLayer.GetAdorners(selectedElement)[0]);
+                            selectedElement = null;
+                        }
+                    }
+                }
+                private void DrawShape(object sender, RoutedEventArgs e)
+                {
+                    this.shapesCount++;
+                    int myValue = Convert.ToInt32(((Button)sender).Tag);
+                    string tmpName = "Shape__" + this.shapesCount;
+                    Shape el;
+                    switch (myValue)
+                    {
+                        case 0:
+                            el = new Ellipse(tmpName);
+                            break;
+                        case 1:
+                            el = new Rectangle(tmpName);
+                            break;
+                        case 2:
+                            el = new Triangle(tmpName);
+                            break;
+                        case 3:
+                            el = new TextBlock(tmpName);
+                            break;
+                        case 4:
+                            el = new ImageBox(tmpName);
+                            break;
+                        default: throw new Exception();
+                    }
+
+                    shapeButton myButton = new shapeButton(tmpName);
+                    this.shapesBar.Add(myButton);
+                    shapeList.Add(el);
+                    UIElement myShape = el.Draw();
+                    this.mainCanvas.Children.Add(myShape);
+                    myButton.CloseSignClicked += (send, eventArgs) =>
+                    {
+                        shapeList.Remove(el);
+                        if (myShape == selectedElement)
+                        {
+                            selectedElement = null;
+                            selected = false;
+                        }
+                        this.mainCanvas.Children.Remove(myShape);
+                        this.shapesBar.Remove(myButton);
+                    };
+                    myButton.SignClicked += (send, eventArgs) =>
+                    {
+                        if (selected)
+                        {
+                            selected = false;
+                            if (selectedElement != null)
+                            {
+                                aLayer.Remove(aLayer.GetAdorners(selectedElement)[0]);
+                                selectedElement = null;
+                            }
+                        }
+
+                        selectedElement = myShape as UIElement;
+
+                        _originalLeft = Canvas.GetLeft(selectedElement);
+                        _originalTop = Canvas.GetTop(selectedElement);
+
+                        aLayer = AdornerLayer.GetAdornerLayer(selectedElement);
+                        aLayer.Add(new MyAdorner(selectedElement));
+                        selected = true;
+                    }; 
+                }
+
+
+                private void ClearCanvas(object sender, RoutedEventArgs e)
+                {
+                    this.mainCanvas.Children.Clear();
+                    shapeList.Clear();
+                    this.shapesBar.Clear();
+                    this.shapesCount = 0;
+                    selected = true;
                     selectedElement = null;
                 }
-            }
-        }
+*/
         private void DrawShape(object sender, RoutedEventArgs e)
         {
-            this.shapesCount++;
-            int myValue = Convert.ToInt32(((Button)sender).Tag);
-            string tmpName = "Shape__" + this.shapesCount;
-            Shape el;
-            switch (myValue)
-            {
-                case 0:
-                    el = new Ellipse(tmpName);
-                    break;
-                case 1:
-                    el = new Rectangle(tmpName);
-                    break;
-                case 2:
-                    el = new Triangle(tmpName);
-                    break;
-                case 3:
-                    el = new TextBlock(tmpName);
-                    break;
-                case 4:
-                    el = new ImageBox(tmpName);
-                    break;
-                default: throw new Exception();
-            }
-
-            shapeButton myButton = new shapeButton(tmpName);
-            this.shapesBar.Add(myButton);
-            shapeList.Add(el);
-            UIElement myShape = el.Draw();
-            this.mainCanvas.Children.Add(myShape);
-            myButton.CloseSignClicked += (send, eventArgs) =>
-            {
-                shapeList.Remove(el);
-                if (myShape == selectedElement)
-                {
-                    selectedElement = null;
-                    selected = false;
-                }
-                this.mainCanvas.Children.Remove(myShape);
-                this.shapesBar.Remove(myButton);
-            };
-            myButton.SignClicked += (send, eventArgs) =>
-            {
-                if (selected)
-                {
-                    selected = false;
-                    if (selectedElement != null)
-                    {
-                        aLayer.Remove(aLayer.GetAdorners(selectedElement)[0]);
-                        selectedElement = null;
-                    }
-                }
-
-                selectedElement = myShape as UIElement;
-
-                _originalLeft = Canvas.GetLeft(selectedElement);
-                _originalTop = Canvas.GetTop(selectedElement);
-
-                aLayer = AdornerLayer.GetAdornerLayer(selectedElement);
-                aLayer.Add(new MyAdorner(selectedElement));
-                selected = true;
-            }; 
         }
-
         private void ClearCanvas(object sender, RoutedEventArgs e)
         {
-            this.mainCanvas.Children.Clear();
-            shapeList.Clear();
-            this.shapesBar.Clear();
-            this.shapesCount = 0;
-            selected = true;
-            selectedElement = null;
         }
-
     }
 }
 
---
+++ ViewModels/MainModelView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApplication1.Models;
+
+namespace WpfApplication1.ViewModels
+{
+    public class MainModelView
+    {
+        private ObservableCollection<ShapeViewModel> _shapes = new ObservableCollection<ShapeViewModel>();
+
+        public MainModelView()
+        {
+            _shapes.Add(new RectangleViewModel(new RectangleModel()));
+            _shapes.Add(new RectangleViewModel(new RectangleModel()));
+            _shapes.Add(new RectangleViewModel(new RectangleModel()));
+        }
+
+        public ObservableCollection<ShapeViewModel> Shapes
+        {
+            get
+            {
+                return _shapes;
+            }
+        }
+    }
+}
